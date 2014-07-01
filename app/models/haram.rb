ac03225765ebf9bbea7d354cc97e28a7a656e723
mahsa <m_mw7462@yahoo.com>
2014-07-01 13:22:41 +0430

    add jquery effect to AccordionBox for show session on home page

--- app/models/haram.rb
+++ app/models/haram.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 class Haram < ActiveRecord::Base
-  has_attached_file :image, :styles => { :large => "500x500>" , :medium => "300x300>", :small => "200x200#" }
+  has_attached_file :image, :styles => { :original => "500x500>" , :medium => "300x300>", :small => "200x200#" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"],
                                             :message =>  "فرمت عکس صحیح نیست"
   def description
