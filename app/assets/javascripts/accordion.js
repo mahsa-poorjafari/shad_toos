7d7373c6737f8612bbb3de62d518b39dec2d77e5
mahsa <m_mw7462@yahoo.com>
2014-07-02 14:21:24 +0430

    add entezar font_family for slider descripton

--- app/assets/javascripts/accordion.js
+++ app/assets/javascripts/accordion.js
@@ -6,10 +6,12 @@ function slideSwitch() {
   var nextone = (AccordionBox.parent().next().children(':first').length == 0) ? $('.AccordionBox ul li:first') : AccordionBox.parent().next().children(':first')
   nextone.addClass('active');
 }
-
-
   
 $(function() { 
   setInterval("slideSwitch()",3000);
+});
+
+$(function() { 
+  setInterval("slideboxes()",3000);
 });
 
