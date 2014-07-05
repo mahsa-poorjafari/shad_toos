26a8ae20d41ff35941d868fc006d2f887be58f9c
mahsa <m_mw7462@yahoo.com>
2014-07-05 18:11:01 +0430

    fix bug for mouse over on accardion

--- app/assets/javascripts/accordion.js
+++ app/assets/javascripts/accordion.js
@@ -1,13 +1,14 @@
 function slideSwitch() {
-  var AccordionBox = $('.AccordionBox ul li.active');
-  AccordionBox.removeClass('active');
-  var nextone = (AccordionBox.parent().next().children(':first').length == 0) ? $('.AccordionBox ul li:first') : AccordionBox.parent().next().children(':first')
-  nextone.addClass('active');
+  if (window.DisableAccordionBox)
+    return
+  
+    var AccordionBox = $('.AccordionBox ul li.active');
+    AccordionBox.removeClass('active');
+    var nextone = (AccordionBox.parent().next().children(':first').length == 0) ? $('.AccordionBox ul li:first') : AccordionBox.parent().next().children(':first');
+    nextone.addClass('active');
+  
 }
   
-$(function() { 
+$(function() {   
   setInterval("slideSwitch()",3000);
 });
-
-
-
