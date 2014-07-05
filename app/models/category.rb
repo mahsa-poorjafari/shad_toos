8a2daf22c457b61c238cc6e1fb80a58ece167510
mahsa <m_mw7462@yahoo.com>
2014-07-05 16:27:51 +0430

    add submenu for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@
 class Category < ActiveRecord::Base
   extend FriendlyId  
   friendly_id :title_en
-  has_attached_file :image, :styles => { :original => "500x500>" , :medium => "300x300>", :small => "200x200#" }
+  has_attached_file :image, :styles => { :original => "500x500>" , :medium => "300x300>", :small => "150x150#" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"],
                                             :message =>  "فرمت عکس صحیح نیست"
                                             
